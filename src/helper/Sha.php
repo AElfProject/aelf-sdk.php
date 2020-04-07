36ed7e89cf246e4bf377880763f1dbb2158c9a85
HeQiaoYu <2774297013@qq.com>
2020-04-07 16:34:02 +0800

    update base58 and curl request file

--- src/helper/Sha.php
+++ src/helper/Sha.php
@@ -31,104 +31,7 @@ function sha256($data, $rawOutput = false)
     $rawOutput = !!$rawOutput;
     return hash('sha256', $data, $rawOutput);
 }
-/* PHP sha256_file() */
-function sha256_file($file, $rawOutput = false)
-{
-    if (!is_scalar($file)) {
-        return false;
-    }
-    $file = (string)$file;
-    if (!is_file($file) || !is_readable($file)) {
-        return false;
-    }
-    $rawOutput = !!$rawOutput;
-    return hash_file('sha256', $file, $rawOutput);
-}
-/**
- * 发送http请求
- * @param string $url 请求地址
- * @param string $method http方法(GET POST PUT DELETE)
- * @param array $data http请求数据
- * @param array $header http请求头
- * @param Int   $type  请求数据类型 0-array  1-jason
- * @return string|bool
- */
-function send_request($url, $method = "POST", $data = array(), $header = array(), $type = '0') {
-    //检查地址是否为空
-    if (empty($url)) {
-        return false;
-    }
-    //控制请求方法范围
-    $httpMethod = array('GET', 'POST', 'PUT', 'DELETE');
-    $method = strtoupper($method);
-    if (!in_array($method, $httpMethod)) {
-        return false;
-    }
-    //请求头初始化
-    $request_headers = array();
-    $User_Agent = 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.43 Safari/537.31';
-    $request_headers[] = 'User-Agent: '. $User_Agent;
-    if($header){
-        foreach ($header as $v) {
-            $request_headers[] = $v;
-        }
-    }
 
-    $request_headers[] = 'Accept: text/html,application/json,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8';
-    switch ($method) {
-        case "POST":
-            $request_headers[] = "X-HTTP-Method-Override: POST";
-            break;
-        case "PUT":
-            $request_headers[] = "X-HTTP-Method-Override: PUT";
-            break;
-        case "DELETE":
-            $request_headers[] = "X-HTTP-Method-Override: DELETE";
-            break;
-        default:
-    }
-    //发送http请求
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_HEADER, false);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);//https
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $request_headers);
-    switch ($method) {
-        case "POST":
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-            break;
-        case "PUT":
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
-            break;
-        case "DELETE":
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
-            break;
-        default:
-    }
-
-    //格式化发送数据
-    if($data) {
-        if ($type) {
-            $dataValue = json_encode($data,JSON_UNESCAPED_UNICODE);
-        }else{
-            $dataValue = http_build_query($data);
-        }
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $dataValue);
-    }
-
-    curl_setopt($ch, CURLOPT_TIMEOUT, 50);
-    //发送请求获取返回响应
-    $result['data'] = curl_exec($ch);
-    $result['httpCode'] = curl_getinfo($ch,CURLINFO_HTTP_CODE);
-    if(strlen(curl_error($ch))>1){
-        $result = false;
-    }
-
-    curl_close($ch);
-    return $result;
-}
 function decodeChecked($address)
 {
     $base58 = new base58();
@@ -156,7 +59,6 @@ function checksum($input){
     return hash('sha256', hex2bin(hash('sha256', $input)));
 }
 
-
 function SHA256Hex($str){
     $re=hash('sha256', $str, true);
     return bin2hex($re);
@@ -165,28 +67,5 @@ function Sha256twice($str){
  
    return hash('sha256', hex2bin(hash('sha256', $str)));
 }
-/* PHP sha512() */
-function sha512($data, $rawOutput = false)
-{
-    if (!is_scalar($data)) {
-        return false;
-    }
-    $data = (string)$data;
-    $rawOutput = !!$rawOutput;
-    return hash('sha512', $data, $rawOutput);
-}
 
-/* PHP sha512_file()*/
-function sha512_file($file, $rawOutput = false)
-{
-    if (!is_scalar($file)) {
-        return false;
-    }
-    $file = (string)$file;
-    if (!is_file($file) || !is_readable($file)) {
-        return false;
-    }
-    $rawOutput = !!$rawOutput;
-    return hash_file('sha512', $file, $rawOutput);
-}
 ?>
